64c5b7d94887cfd985d762abc93c26edf50b0bad
f3lixh <47897037+f3lixh@users.noreply.github.com>
2024-06-11 00:36:33 +0200

    update homepage

--- src/components/TaskOne/ChartThree.tsx
+++ src/components/TaskOne/ChartThree.tsx
@@ -4,7 +4,7 @@ import BarChart from "../BarChart/BarChart";
 const ChartOne = () => {
     return (
         <>
-            <h1>Entlassungen in der Videospielindustrie </h1>
+            <h1>Entlassungen in der Videospielindustrie der letzten Jahre</h1>
             <br/>
             <br/>
             <BarChart label1="2023" value1={11250} label2="2024 (Jan-April)" value2={8800} />
